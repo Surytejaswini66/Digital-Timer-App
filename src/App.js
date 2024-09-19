44090ffd56267e19a389a2291e1c0c4ec1fcc9e4
Surytejaswini66 <suryaviswanadhapalli666@gmail.com>
2024-09-19 11:45:14 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import DigitalTimer from './components/DigitalTimer'
+
+const App = () => {
+  return (
+    <div className="App">
+      <DigitalTimer />
+    </div>
+  )
+}
+
+export default App
